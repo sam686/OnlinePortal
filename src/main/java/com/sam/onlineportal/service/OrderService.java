2b83556a6d91bad0283e86f2162b9c86dcf28623
Samadrita Kundu Mitra <samadritakundu_mitra@persistent.co.in>
2016-12-05 19:30:20 +0530

    Changed approach

--- src/main/java/com/sam/onlineportal/service/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.sam.onlineportal.service;
-
-import com.sam.onlineportal.bo.Cart;
-import com.sam.onlineportal.entity.Order;
-
-public interface OrderService {
-	
-	public Order createOrder(Cart cart);
-}
